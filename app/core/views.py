883a31a2441ae9b05d35c98a15077a324cd4b3de
EdwinKassier <edwinkassier@gmail.com>
2022-09-23 11:11:08 +0200

    Test

--- app/core/views.py
+++ app/core/views.py
@@ -16,7 +16,7 @@ logger = LocalProxy(lambda: current_app.logger)
 def before_request_func():
     current_app.logger.name = 'core'
 
-#Preparing for prod release
+#Preparing for prod release cloud run
 @core.route('/process_request', methods=['GET'])
 def test():
     logger.info('app test route hit')
