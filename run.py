a5050ef9560514f155f6cdd251f110c31e46ec26
EdwinKassier <edwinkassier@gmail.com>
2022-09-23 11:44:14 +0200

    Update port again

--- run.py
+++ run.py
@@ -17,4 +17,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get("PORT", 8080)))
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
